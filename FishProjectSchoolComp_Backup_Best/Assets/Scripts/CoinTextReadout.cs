eefc7997d23024eb21e579c4b744cb451ab8b749
Ryley Burnett <ryleyburnett@live.com>
2018-12-04 21:37:45 -0700

    Added Scriptable Object Coin Counter
    Includes displays etc.

---
+++ FishProjectSchoolComp_Backup_Best/Assets/Scripts/CoinTextReadout.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinTextReadout : MonoBehaviour 
+{
+
+	private Text label;
+	public ScriptableObjectCoin coinCounter;
+	private string coins;
+
+	
+	void Update()
+	{
+		
+		label = GetComponent<Text>();
+
+		coins = coinCounter.coinCount.ToString();
+		label.text = coins;
+		
+	} 
+	
+}
--- FishProjectSchoolComp_Backup_Best/Assets/Scripts/RunCountdown.cs
+++ FishProjectSchoolComp_Backup_Best/Assets/Scripts/RunCountdown.cs
@@ -7,9 +7,14 @@ public class RunCountdown : MonoBehaviour
 {
 
 	private Text label;
+	public int scoreReadout;
+	public ScriptableObjectCoin coinCounter;
+
 	
+
 	IEnumerator Start ()
 	{
+		
 		label = GetComponent<Text>();
 
 		int number = 1;
@@ -19,12 +24,31 @@ public class RunCountdown : MonoBehaviour
 			yield return new WaitForSeconds(0.025f);
 			label.text = number.ToString();
 			number++;
+			
+			
+			coinCounter.score = number;
+		
+
 
 		}
-
+	
+	
 		//label.text = "";
 		//yield return new WaitForSeconds(1);
 		//label.text = "Go!";
 	}
+
+ 
+ 
+	void Update()
+	{
+		
+		if(coinCounter.score % 100 == 0)
+		{
+			coinCounter.coinCount++;
+		}
+		
+	} 
+	
 	
 }
